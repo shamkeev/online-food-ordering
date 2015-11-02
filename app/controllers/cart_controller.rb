f35dd83121abd57328f746d0312b2759a2449eab
shamkeev <shamkeyev@mail.ru>
2015-11-02 19:08:57 +0600

    Added cart/add post method route.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,7 +1,14 @@
 class CartController < ApplicationController
 
   def add
+    #Возможно это нужно вынести в метод, и вызывать в before acction, чтобы session[:cart] всегда существовало
+    unless session[:cart]
+      session[:cart] = []
+    end
 
+    session[:cart].push params[:meal]
+
+    redirect_to :back
   end
 
 end
