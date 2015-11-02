818229abc162874f21340443288c604c6e6c0962
shamkeev <shamkeyev@mail.ru>
2015-11-02 19:44:35 +0600

    Added faker and paperclip gems to Gemfile Created some seeds

--- Gemfile
+++ Gemfile
@@ -28,15 +28,9 @@ gem 'devise'
 
 gem 'activeadmin', '~>1.0.0.pre2'
 
+gem 'faker'
 
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
-# Use Unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
+gem 'paperclip', '~> 4.3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name:'Administrator', email:'admin@test.com',
+             password:'12345678', password_confirmation:'12345678' )
+
+5.times do
+  Restaurant.create!(name: Faker::Address.street_name, description: Faker::Lorem.sentence)
+end
+
+20.times do
+  Meal.create!(name: Faker::Commerce.product_name, restaurant: Faker::Number.between(1, 5),
+               price: Faker::Commerce.price, description: Faker::Lorem.sentence)
+end
